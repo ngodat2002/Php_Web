9bf4e4fd8ed84b850b7cfbc2970bfa1c3c36f25e
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2021-08-20 14:09:39 +0700

    Update index.php

--- Exercies/Books/index.php
+++ Exercies/Books/index.php
@@ -62,17 +62,6 @@
                             echo "<td>" .$row['ISBN'] . "</td>";
                             echo "<td>" .$row['pub_year'] . "</td>";
                             echo "<td>" .$row['available'] . "</td>";
-//                            echo "<td>";
-//                            echo "<a href='read.php?id=".$row['id']
-//                                ."' title='View Record' data-toggle='tooltip'>
-//                                        <span class='glyphicon glyphicon-eye-open'></span></a>";
-//                            echo "<a href='update.php?id=".$row['id']
-//                                ."' title='Update Record' data-toggle='tooltip'>
-//                                         <span class='glyphicon glyphicon-pencil'></span></a>";
-//                            echo "<a href='delete.php?id=".$row['id']
-//                                ."' title='View Record' data-toggle='tooltip'>
-//                                        <span class='glyphicon glyphicon-trash'></span></a>";
-//                            echo "</td>";
                             echo "</tr>";
                         }
                         echo "</tbody>";
